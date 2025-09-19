ec20727de7f3a49909a13b2b0c4e1fa7b7d72959
Faraway-Yachting <info@faraway-yachting.com>
2025-09-19 18:07:51 +0500

    5656

---
+++ src/app/layout.tsx
@@ -0,0 +1,73 @@
+import {DM_Serif_Display} from "next/font/google";
+import {Plus_Jakarta_Sans} from "next/font/google";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./globals.css";
+import Navbar from "@/Component/Navbar";
+import Header from "@/Component/Header";
+import Licence from "@/Component/Licence";
+import Footer from "@/Component/Footer";
+import WhatsAppIcon from "@/Component/WhatsAppIcon";
+import GoogleAnalyticsComponent from "@/Component/GoogleAnalytics";
+const DMSerifDisplay = DM_Serif_Display({
+  variable: "--font-dm-serif-display",
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  preload: true,
+});
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  preload: true,
+});
+// Keep existing variables but use the main Plus Jakarta Sans font
+const geistSans = plusJakartaSans;
+const geistMono = plusJakartaSans;
+const playfairDisplay = DMSerifDisplay;
+const source_Sans_Pro = plusJakartaSans;
+const inter =  plusJakartaSans;
+const poppins = plusJakartaSans;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RVTYFBSDC6"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RVTYFBSDC6');
+            `,
+          }}
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} ${source_Sans_Pro.variable}  ${inter.variable}  ${poppins.variable}  antialiased`}
+      >
+        <div className="fixed top-0 z-40 w-full">
+          <Header />
+          <Navbar />
+        </div>
+        <main className="flex-1 relative pt-[5rem] md:pt-[8rem]">
+          {children}
+        </main>
+        <Licence />
+        <Footer />
+        <WhatsAppIcon />
+        <GoogleAnalyticsComponent />
+      </body>
+    </html>
+  );
+}
