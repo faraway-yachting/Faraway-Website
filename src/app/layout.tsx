bf90db199174cc8d12859bb13f08fe9263c6b0a7
Jawad Akram <info@faraway-yachting.com>
2025-09-20 20:11:16 +0500

    22

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#D6AB61" />
+        <link rel="manifest" href="/site.webmanifest" />
+        
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
